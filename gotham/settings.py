ef65a6fb4189241faf236d7e51bc54490d771990
himanshu <himanshu@paasresearch.com>
2021-05-30 01:38:17 +0530

    database settings updated

--- gotham/settings.py
+++ gotham/settings.py
@@ -10,10 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from .local_settings import *
 
 
 # Quick-start development settings - unsuitable for production
@@ -69,17 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'gotham.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
